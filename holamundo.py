d3582707367cfb8deaeb03d711df0f8a0807561d
jumoreno <ceinnova00137@innovaccion.mx>
2022-06-13 14:59:04 -0700

    initial commit

---
+++ holamundo.py
@@ -0,0 +1,25 @@
+print("hola mundo")
+name = input ("Cual es tu nombre?  ")
+print(f"hola " + name)
+edad = input("Caul es tu edad? ")
+edad = int(edad)
+print(f"hola " + name, "de ", edad, " años")
+if edad >= 18 and edad < 99:
+    print("!bienvenido al Play Boy!!")
+    id = input("tienes identificacion? ")
+    if id == 'no':
+        print("casi la pegas wer@")
+    else: print("pasale, ponte comodo y disfruta el show...")
+    print("desea cambio de billetes de 1 dollar?")
+    print("nosotros le podemos cambar")
+    pesos = input("cuantos pesos quiere cambiar?")
+    pesos = int(pesos)
+    dollar = 20
+    billetes = pesos / dollar
+    billetes = int(billetes)
+    billetes = str(billetes)
+    print(f"son: " + billetes, "de 1 dollar")
+elif edad > 99:
+    print("pase bajo su propio riesgo...")
+else:
+    print("no puede pasar, prohibida la entrada a menores!")
