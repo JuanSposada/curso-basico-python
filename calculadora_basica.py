768bcad90d868738833e0d4e97f5fab5423783c8
JuanSposada <ceinnova00137@innovaccion.mx>
2022-07-05 11:03:55 -0700

    first commit

---
+++ calculadora_basica.py
@@ -0,0 +1,13 @@
+"""este programa calcula las opreaciones basicas de dos valores introducido por el usuario"""
+
+# Entrada de variables
+
+valor_1 = int(input("ingrese el primer valor: "))
+valor_2 = int(input("ingrese el segundo valor: "))
+
+print('esta es la suma de los varoles: ', valor_1 + valor_2)
+print('esta es la resta de los valores: ', valor_1 - valor_2)
+print('esta es la multiplicacion de los valores: ', valor_1 * valor_2)
+print('Esta es la division de los valores: ', valor_1 / valor_2)
+print('esta es la division entera de los valores: ', valor_1 // valor_2)
+print('este es el modulo de los valores: ', valor_1 % valor_2)
