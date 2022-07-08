7f05077c164166c9bc01dd13d19818ce1fc43893
JuanSposada <ceinnova00137@innovaccion.mx>
2022-07-08 12:51:45 -0700

    first commit

---
+++ bandera.py
@@ -0,0 +1,37 @@
+#este programa te muestra la bandera de un pais
+
+ 
+
+print(" \nEstos son los paises disponibles: \nPiratebay, Racingland, Rainbowland y Blackland \n")
+
+ 
+
+#entrada de datos
+
+ 
+
+ 
+
+pais = input('ingresa un pais: ')
+
+ 
+
+if pais == 'Racingland':
+
+    print('esta es su bandera 🏁')
+
+elif pais == 'Rainbowland':
+
+    print('esta es su bandera 🏳‍🌈')
+
+elif pais == 'Piratebay':
+
+    print('esta es su bandera 🏴‍☠️')
+
+elif pais == 'Blackland':
+
+    print('esta es su bandera 🏴')
+
+else: print("ese no es un pais valido 🇲🇽")
+
+   
