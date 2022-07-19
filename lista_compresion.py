56e63969c0b63605b8214a06edb5670c4fe9ddd8
JuanSposada <ceinnova00137@innovaccion.mx>
2022-07-19 09:45:13 -0700

    first commit

---
+++ lista_compresion.py
@@ -0,0 +1,21 @@
+""" este programa crea una lista
+
+con el resultado de aplicar una funcion, utilizando
+
+listas por comprension"""
+
+ 
+
+ 
+
+x = [int(v) for v in range(20)]
+
+print(x)
+
+ 
+
+fx =[(3 * value + 2) for value in x]
+
+print(fx)
+
+ 
